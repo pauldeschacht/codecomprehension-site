28655d6f6cf5180429600f41a2f468b908ea7ff2
pauldeschacht <pauldeschacht@gmail.com>
2021-03-02 18:12:50 +0100

    added landing page redesigned survey

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
+    "./index.html",
+    "./survey.html"
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+
+      primary: colors.lightBlue,
+      secondary: colors.emerald
+    },
+    extend: {
+      backgroundImage: theme => ({
+        'hero-pattern': "url('/img/bg.jpg')",
+      }),
+    }
   },
   variants: {
     extend: {},
